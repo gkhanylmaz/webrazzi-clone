7a2bf683ed26926a8c74561672a9e79565a9d733
Gökhan Yılmaz <gokhanyilmaz997@gmail.com>
2022-05-27 17:57:53 +0300

    netlify

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -16,29 +16,33 @@ import styles from "./HomePage.module.scss";
 
 const HomePage = ({ data }) => {
   return (
-    <div className={styles.container}>
-      <div style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}>
-        <Home data={data.slice(0, 1)} />
-        <Highlights data={data} />
+    <div className={styles.HomePage}>
+      <div className={styles.container}>
+        <div
+          style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}
+        >
+          <Home data={data.slice(0, 1)} />
+          <Highlights data={data} />
+        </div>
+        <Added />
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "2fr 1fr",
+            marginTop: "100px",
+          }}
+        >
+          <ListCards data={data.slice(6, 13)} />
+          <Popular data={data.slice(13, 18)} />
+        </div>
+        <WbTv data={data.slice(19, 23)} />
+        <ListCards data={data.slice(23, 29)} />
+        <WbActivity data={data.slice(29, 32)} />
+        <ListCards data={data.slice(32, 38)} />
+        <WbKariyer data={data.slice(38, 41)} />
+        <ListCards data={data.slice(41, 46)} />
+        <Buttons data={data} />
       </div>
-      <Added />
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "2fr 1fr",
-          marginTop: "100px",
-        }}
-      >
-        <ListCards data={data.slice(6, 13)} />
-        <Popular data={data.slice(13, 18)} />
-      </div>
-      <WbTv data={data.slice(19, 23)} />
-      <ListCards data={data.slice(23, 29)} />
-      <WbActivity data={data.slice(29, 32)} />
-      <ListCards data={data.slice(32, 38)} />
-      <WbKariyer data={data.slice(38, 41)} />
-      <ListCards data={data.slice(41, 46)} />
-      <Buttons data={data} />
     </div>
   );
 };
